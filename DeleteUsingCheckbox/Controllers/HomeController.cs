04b6aa207ee0f88cc41aa9f710185e50fb86085e
shubhamrokade <shubhamrokade211@gmail.com>
2019-08-22 15:44:10 +0530

    m

--- DeleteUsingCheckbox/Controllers/HomeController.cs
+++ DeleteUsingCheckbox/Controllers/HomeController.cs
@@ -40,6 +40,19 @@ namespace DeleteUsingCheckbox.Controllers
             entities.SaveChanges();
             return RedirectToAction("Index");
         }
+        public ActionResult Edit(int id)
+        {
+            var emp = entities.Employees.FirstOrDefault(x=>x.UserId==id);
+            return View(emp);
+        }
 
+        [HttpPost]
+        public ActionResult Edit(Employee emp)
+        {
+            entities.Entry(emp).State = System.Data.Entity.EntityState.Modified;
+            entities.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
     }
 }
