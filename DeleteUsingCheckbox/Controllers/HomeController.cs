021e98f7b5629dae6e3f9a049759346175ab988f
shubhamr1 <shubhamr1@SMD-0854>
2019-08-22 12:37:43 +0530

    Add project files.

---
+++ DeleteUsingCheckbox/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DeleteUsingCheckbox.Models;
+
+namespace DeleteUsingCheckbox.Controllers
+{
+    public class HomeController : Controller
+    {
+        EmployeeEntities entities = new EmployeeEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.ListEmployee = entities.Employees.ToList();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(FormCollection formCollection)
+        {
+            string[] ids = formCollection["UserId"].Split(new char[] { ',' });
+            foreach (string id in ids)
+            {
+                var employee = entities.Employees.Find(int.Parse(id));
+                entities.Employees.Remove(employee);
+                entities.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
